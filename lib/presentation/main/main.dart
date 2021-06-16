308ceb4cc64ee929a913be2d64625035475b91ce
dtanakab <karugamo.highkick@gmail.com>
2021-06-16 23:51:20 +0900

    仮でフッターを追加

--- lib/presentation/main/main.dart
+++ lib/presentation/main/main.dart
@@ -16,43 +16,65 @@ class App extends StatelessWidget {
         home: ChangeNotifierProvider<MainModel>(
           create: (_) => MainModel(),
           child: Scaffold(
-              appBar: AppBar(
-                title: Text('デモ'),
-              ),
-              body: Consumer<MainModel>(builder: (context, model, child) {
-                return Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        model.displayText,
-                        style: TextStyle(
-                          fontSize: 30,
-                        ),
+            appBar: AppBar(
+              title: Text('デモ'),
+            ),
+            body: Consumer<MainModel>(builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: [
+                    Text(
+                      model.displayText,
+                      style: TextStyle(
+                        fontSize: 30,
                       ),
-                      RaisedButton(
-                        child: Text('新規登録'),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignUpPage()),
-                          );
-                        },
-                      ),
-                      RaisedButton(
-                        child: Text('番組一覧'),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ProgramListPage()),
-                          );
-                        },
-                      )
-                    ],
-                  ),
-                );
-              })),
+                    ),
+                    RaisedButton(
+                      child: Text('新規登録'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignUpPage()),
+                        );
+                      },
+                    ),
+                    RaisedButton(
+                      child: Text('番組一覧'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProgramListPage()),
+                        );
+                      },
+                    )
+                  ],
+                ),
+              );
+            }),
+            bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'ホーム',
+                    backgroundColor: Colors.orange),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.list),
+                    label: 'リスト',
+                    backgroundColor: Colors.orange),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search),
+                    label: 'さがす',
+                    backgroundColor: Colors.orange),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.settings),
+                    label: 'マイページ',
+                    backgroundColor: Colors.orange),
+              ],
+              currentIndex: 2,
+              selectedItemColor: Colors.white,
+            ),
+          ),
         ));
   }
 }
